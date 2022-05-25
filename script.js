4fa09f26951f9a4e6c9e737f8d112c724ee713d5
Pedro Nunes <pedro.icn88@gmail.com>
2022-05-25 16:06:10 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // cria quadro de divs
 
-for(let i = 1; i <= 25; i += 1){
-  let div = document.createElement('div')
-  div.className = 'pixel'
-  document.getElementById('pixel-board').appendChild(div)
+for(let i = 1; i <= 25; i += 1) {
+  let div = document.createElement('div');
+  div.className = 'pixel';
+  document.getElementById('pixel-board').appendChild(div);
+}
+
+window.onload = selecionaCorInicial
+
+function selecionaCorInicial() {
+  document.getElementById('black').className = 'color selected'
 }
